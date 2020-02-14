3a80993920dbd72e92dccf3c929c6a0158858595
NelsonVrN <vidalnelson871@gmail.com>
2020-02-14 08:29:08 -0300

    idade...

--- teste.c
+++ teste.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 
 int main(){
+    int idade;
+    printf("Digite a idade do usuario");
+    scanf("%i",&idade);
+    idade -= 1;
+    printf("idade do usuario menos 1:%i ",idade);
 
 
     return 0;
